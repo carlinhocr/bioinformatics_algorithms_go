41ac633579c31f1ff5f2dd5d4af82d848ea3f64e
carlinhocr <carlos.rienzi@gmail.com>
2022-02-08 17:15:11 -0300

    more functions

--- Bioinformatics1_FindingHiddenMessagesinDNA/main.go
+++ Bioinformatics1_FindingHiddenMessagesinDNA/main.go
@@ -3,14 +3,6 @@ package main
 
 import "fmt"
 
-//def patternCount(self, text, pattern):
-//	count = 0
-//	for i in range(0,(len(text)-len(pattern))+1):
-//		subtext = text[i:len(pattern)+i]
-//		if subtext == pattern:
-//			count +=1
-//	return count
-
 func patternCount(text string, pattern string) int {
 	count := 0
 	n := len(text)
@@ -24,10 +16,39 @@ func patternCount(text string, pattern string) int {
 	return count
 }
 
+//def frequencyTable(self, text="hola", k=0):
+//	freqMap = {}
+//	n = len(text)
+//	for i in range(0, n - k):
+//		pattern = text[i:k+i]
+//		if not pattern in freqMap:
+//			freqMap[pattern]=1
+//		else:
+//			freqMap[pattern] += 1
+//	return freqMap
+
+//def MaxMap(self,freqMap):
+//	maximum = 0
+//	for element in freqMap:
+//		if abs(freqMap[element]) > maximum:
+//			maximum = abs(freqMap[element])
+//	return maximum
+
+//def BetterFrequentWords(self,text,k):
+//	frequentPatterns = []
+//	freqMap = self.frequencyTable(text,k)
+//	max = self.MaxMap(freqMap)
+//	for pattern in freqMap:
+//		# the patern is the key in the dictionary, the value it is how often it is found in text
+//		if freqMap[pattern] == max:
+//			frequentPatterns.append(pattern)
+//	return frequentPatterns
+
 func main() {
 	fmt.Println("Here to expect Bioinformatics Code in Golang")
-	// Exercise 1
+	// Exercise 1 PatternCount
 	text := "GCGCG"
 	pattern := "GCG"
 	fmt.Println(patternCount(text, pattern))
+	// Exercise 2 BetterFrequentWords
 }
