8c13b166915ae6d2f0f5e0d8d5c1ba3ac874122f
carlinhocr <carlos.rienzi@gmail.com>
2022-02-08 15:54:17 -0300

    first function from python to go

--- Bioinformatics1_FindingHiddenMessagesinDNA/main.go
+++ Bioinformatics1_FindingHiddenMessagesinDNA/main.go
@@ -3,7 +3,30 @@ package main
 
 import "fmt"
 
+//def patternCount(self, text, pattern):
+//	count = 0
+//	for i in range(0,(len(text)-len(pattern))+1):
+//		subtext = text[i:len(pattern)+i]
+//		if subtext == pattern:
+//			count +=1
+//	return count
+
+func patternCount(text string, pattern string) int {
+	count := 0
+	n := len(text)
+	k := len(pattern)
+	for i := 0; i < (n-k)+1; i++ {
+		subtext := text[i : i+k]
+		if subtext == pattern {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
 	fmt.Println("Here to expect Bioinformatics Code in Golang")
-
+	text := "GCGCG"
+	pattern := "GCG"
+	fmt.Println(patternCount(text, pattern))
 }
